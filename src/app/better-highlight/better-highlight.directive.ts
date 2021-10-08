cd354e2c7e2fef795b37ca782524c5d8984ac6ca
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-08 16:36:26 -0400

    using HostListener to listen to host events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, RendererStyleFlags2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,11 +7,22 @@ import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
 // The Renderer2 class is an abstraction provided by Angular in the form of a service that allows to manipulate elements of your app without having to touch the DOM directly. This is the recommended approach because it then makes it easier to develop apps that can be rendered in environments that don’t have DOM access, like on the server, in a web worker or on native mobile.
 
 export class BetterHighlightDirective {
+  flags = RendererStyleFlags2.DashCase | RendererStyleFlags2.Important;
+
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+
+
+  @HostListener('mouseenter')mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', RendererStyleFlags2.Important)
+  }
+
+  @HostListener('mouseleave')mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent', RendererStyleFlags2.Important);
   }
 
 }
